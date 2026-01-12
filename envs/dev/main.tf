298aaa395dfc628dbc77aa613748ea0eb29e6b22
aswani kumar <77104321+ashwin-ai@users.noreply.github.com>
2026-01-12 12:29:57 +0530

    Add assume_role block to AWS provider configuration

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,4 +1,9 @@
 provider "aws" {
   region = var.aws_region
-}
 
+  assume_role {
+    # ARN of the IAM role you created
+    role_arn     = "arn:aws:iam::982534376194:role/terraform-prod-role"
+    session_name = "terraform-prod-session"
+  }
+}
