5d8e96a842d308d08f4db31e935116586374421d
aswani kumar <77104321+ashwin-ai@users.noreply.github.com>
2026-01-13 11:03:34 +0530

    Enhance aws_instance resource with connection and provisioner
    Added SSH connection and remote provisioning for EC2 instance.

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,10 +1,28 @@
 resource "aws_instance" "terraform_ec2" {
-  ami                    = "ami-0c02fb55956c7d316"
-  instance_type          = var.instance_type
-  key_name               = var.key_name
+  ami                         = "ami-0c02fb55956c7d316"
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
   associate_public_ip_address = true
 
   tags = {
     Name = "terraform-amazon-linux-2023"
+  }
+
+  # 🔐 SSH connection
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("keypair-new.pem")
+    host        = self.public_ip
+  }
+
+  # 🚀 Remote provisioning
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install -y dnf-plugins-core",
+      "sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo",
+      "sudo dnf install -y terraform",
+      "terraform -version"
+    ]
   }
 }
